b653a3aefbee2352bb14361b699da68b9415d7d1
vishal-singh6 <102349146+vishal-singh6@users.noreply.github.com>
2023-01-02 12:18:03 +0530

    Made WalksController

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository,IMapper mapper
+            )
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalksAsync() {
+
+            // Fetch data from database - domain walks
+          var walksDomain =  await walkRepository.GetAllAsync();
+
+            //Convert domain walks to DTO Walks 
+
+            var walkDTO = mapper.Map<List<Models.DTO.Walk>>(walksDomain);
+            // Return Response
+            return Ok(walkDTO);    
+        }
+    }
+}
---
+++ NZWalks.API/Repositories/WalkRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkRepository :IWalkRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public WalkRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<Walk>> GetAllAsync() { 
+            return await nZWalksDbContext.Walks.ToListAsync();
+        }
+    }
+}
